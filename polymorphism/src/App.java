232d5f941e88f3353843cd2052dcd07530d74fe7
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2024-07-18 14:56:51 +0300

    Initial commit

---
+++ polymorphism/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // UPCASTING
+
+        /*
+         * Alt referansı üst referansa eşitlemektir.
+         * 
+         * Her zaman güvnelidir ama tam tersi değildir.
+         *
+         */
+
+        /*
+         * Polimorfizm sayesinde arayüz ile gerçekleştirmeyi ayırabiliriz.
+         */
+
+        // Kodu genel yazmak gerekiyor
+
+        // ************************************************ //
+
+        // Polimorfik metodlar
+        /*
+         * Bir metodun bir arayüzü olur ama birden fazla implementation'a sahip
+         * olabilir.
+         */
+    }
+}
