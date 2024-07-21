490922f379dc7510694f66aebe098a19ad064a3c
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2024-07-21 14:00:25 +0300

    abstract_class

---
+++ abstract_class/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * Soyut sınıfa iki türlü yaklaşılabilir
+         * Nesneler üzerinden
+         * Soyut metotlar üzerinden
+         */
+
+        /* Nesneler üzerinden */
+        /*
+         * Şablon olarak dursun diye ihtiyacımız var.
+         * Şekildeki gibi employee'nin sınıfına ihtiyaç var ama nesnesine ihtiyaç yok.
+         * Hiyerarşinin en tepesinde yer alır ve alt sınıflar için bir kalıp oluşturmak
+         * için vardırlar
+         */
+
+        /* Metotlar üzerinden */
+        /*
+         * Soyut sınıfta arayüz olsun implementation'ı olmasın.
+         * Abstract metot private olamaz.
+         * Abstract sınıf final olamaz.
+         * Abstract sınıfta main metodu olabilir.
+         * Abstract metotta synchronized anahtar kelimesi kullanılamaz.
+         */
+    }
+
+}
